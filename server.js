d32318a5fb0da09ee6ebec17fbd3c03fa2ce468e
Lauren Plenger <laurenb08@gmail.com>
2021-02-27 15:33:05 -0800

    added all new starter files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//     useNewUrlParser: true,
+//     useFindAndModify: false
+// });
+
+// app.use(require("./routes/htmlRoutes"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
