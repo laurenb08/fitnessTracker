395eaa4d4bbc1fac59a144351580aebc25510467
Lauren Plenger <laurenb08@gmail.com>
2021-03-09 21:33:06 -0800

    added routes for finding a workout, a range of workouts, and deleting a workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -26,3 +26,36 @@ router.put("/api/workouts/:id", ({body, params}, res) => {
         res.json(err);
     });
 });
+
+//find workout
+router.get("/api.workouts", (req, res) => {
+    Workout.find()
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+//find range of workouts
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+//delete workout
+router.delete("/api/workouts", ({body}, res) => {
+    Workout.findByIdAndDelete(body.id)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
